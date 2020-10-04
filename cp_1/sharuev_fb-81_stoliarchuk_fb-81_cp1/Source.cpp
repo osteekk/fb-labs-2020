24ab052d2a910b61ae90ae7480fac761c3f3542b
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2020-10-04 22:13:56 +0300

    H1 Entropy

--- cp_1/sharuev_fb-81_stoliarchuk_fb-81_cp1/Source.cpp
+++ cp_1/sharuev_fb-81_stoliarchuk_fb-81_cp1/Source.cpp
@@ -32,6 +32,38 @@ string whitepaper(string text)
 	return newText;
 }
 
+double Entropy_H1(int* alphabet, int length)
+{
+	double P = 0;//pi*log2(pi)
+	double p;//вероятность
+	cout << "Вероятность букв" << endl;
+	wcout << L"а\t б\t в\t г\t д\t е\t ё\t ж\t з\t и\t й" << endl;
+	for (int i = 0; i < 33; i++)
+	{
+		p = (double)alphabet[i] / length;
+		cout << fixed;
+		cout.precision(5);// х,ххххх
+		cout << p << "\t";
+
+		if (i == 10)
+		{
+			wcout << endl << L"к\t л\t м\t н\t о\t п\t р\t с\t т\t у\t ф" << endl;
+		}
+		if (i == 21)
+		{
+			wcout << endl << L"х\t ц\t ч\t ш\t щ\t ъ\t ы\t ь\t э\t ю\t я" << endl;
+		}
+		if (p != 0)
+		{
+			P += (p * log2(p));
+		}
+	}
+	cout << endl;
+	return(P * (-1));
+}
+
+
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
